aa2053d7b923cf4a433b265a7f961eb6c502e6ab
Errol McFadden <errolmcfadden@hotmail.com>
2026-01-29 11:25:50 +1000

    - Fixed next project loading

--- BookingSystem.AppHost/AppHost.cs
+++ BookingSystem.AppHost/AppHost.cs
@@ -9,14 +9,22 @@ if (!string.IsNullOrEmpty(imageRegistry))
     mongoBuilder = mongoBuilder.WithImageRegistry(imageRegistry);
 }
 
-builder.AddProject<Projects.BookingSystem_API>("bookingsystem-api", launchProfileName: null)
-       .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-       .WithHttpEndpoint(port: 5080, isProxied: false) // http://localhost:5080
-       .WithUrlForEndpoint("http", url =>
-        {
-            url.Url = "/scalar";
-            url.DisplayText = "API Docs (Scalar)";
-        })
-       .WithReference(mongoBuilder);
+var api = builder.AddProject<Projects.BookingSystem_API>("bookingsystem-api", launchProfileName: null)
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+    .WithHttpEndpoint(port: 5080, isProxied: false) // http://localhost:5080
+    .WithUrlForEndpoint("http", url =>
+    {
+        url.Url = "/scalar";
+        url.DisplayText = "API Docs (Scalar)";
+    })
+    .WithReference(mongoBuilder);
+
+var frontEnd = builder.AddNpmApp("frontend", "../bookingsystem.web", "dev")
+    .WithHttpEndpoint(port: 3000, env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithReference(api)
+    .WithEnvironment("CHOKIDAR_USEPOLLING", "true")
+    .WithEnvironment("WATCHPACK_POLLING", "true")
+    .WithEnvironment("WATCHPACK_POLLING_INTERVAL", "200");
 
 builder.Build().Run();
