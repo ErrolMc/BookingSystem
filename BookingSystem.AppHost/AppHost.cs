f2e3f33b44954b9761e00a9eefd998cb003af0e4
Errol McFadden <errolmcfadden@hotmail.com>
2026-01-29 10:29:32 +1000

    - Fixed mongo to use custom image registry if there is one

--- BookingSystem.AppHost/AppHost.cs
+++ BookingSystem.AppHost/AppHost.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mongoBuilder = builder.AddMongoDB("mongo");
+
+// Only use custom registry if configured in user secrets (e.g. on corporate network)
+var imageRegistry = builder.Configuration["DockerImageRegistry"];
+if (!string.IsNullOrEmpty(imageRegistry))
+{
+    mongoBuilder = mongoBuilder.WithImageRegistry(imageRegistry);
+}
+
 builder.AddProject<Projects.BookingSystem_API>("bookingsystem-api", launchProfileName: null)
        .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
        .WithHttpEndpoint(port: 5080, isProxied: false) // http://localhost:5080
@@ -8,6 +17,6 @@ builder.AddProject<Projects.BookingSystem_API>("bookingsystem-api", launchProfil
             url.Url = "/scalar";
             url.DisplayText = "API Docs (Scalar)";
         })
-       .WithEnvironment("ConnectionStrings__mongo", "mongodb://localhost:27017");
+       .WithReference(mongoBuilder);
 
 builder.Build().Run();
